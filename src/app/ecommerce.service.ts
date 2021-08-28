b19fb186128234fdd742e049688bea095708b2c5
DevHarry066 <sharmahariom066@gmail.com>
2021-08-28 12:31:03 +0530

    28-08-21

--- src/app/ecommerce.service.ts
+++ src/app/ecommerce.service.ts
@@ -9,14 +9,53 @@ import { environment } from '../environments/environment';
 export class EcommerceService {
 
   private url: string;
+  private productId:any;
 
  private API_URL= environment.API_URL;
 
   constructor(private http:HttpClient) {
+    this.setId(2);
     this.url=this.API_URL;
   }
+
+
+  setId(data:number):Observable<any>
+  {
+    this.productId=data;
+    // return this.productId;
+   console.log("set product Id as "+ this.productId);
+
+    return this.productId;
+  }
+
+  getId()
+  {
+    return this.productId;
+  }
+
+
+
 
   getDealOfDayList(): Observable<any> {
     return this.http.get<any> (this.url+'/api/DealBestSeller');
 }
+
+getBestSellerList(): Observable<any> {
+  return this.http.get<any> (this.url+'/api/DealBestSeller/GetBestSeller');
+}
+
+getProductList(): Observable<any> {
+  return this.http.get<any> (this.url+'/api/Products');
+}
+
+getProductDetails(): Observable<any> {
+  return this.http.get<any> (this.url+'/api/Products/'+this.productId);
+}
+
+
+getProductCategory(data:any): Observable<any> {
+  return this.http.get<any>(this.url+'/api/Categories/FindEmployee?category='+data);
+}
+
+
 }
